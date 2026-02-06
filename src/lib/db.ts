0d320810dc4fa7acbd75c86b57766123d02f9ed5
cxkw.dev <cxkw.dev@gmail.com>
2026-02-05 20:13:57 -0700

    remove prisma query logging in dev

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -13,7 +13,7 @@ function createPrismaClient() {
     adapter,
     log:
       process.env.NODE_ENV === 'development'
-        ? ['query', 'error', 'warn']
+        ? ['error', 'warn']
         : ['error'],
   })
 }
